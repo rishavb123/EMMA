07961213959a371351b43a5a14a201276eae9054
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-04-04 15:18:22 -0400

    moved callback and added poi loggin

--- emma/experiment.py
+++ emma/experiment.py
@@ -3,9 +3,73 @@ from dataclasses import dataclass, field
 import hydra
 from hydra.core.config_store import ConfigStore
 from omegaconf import MISSING
+import logging
+import wandb
+
+from stable_baselines3.common.callbacks import BaseCallback
 
 from experiment_lab.experiments.rl import RLConfig, RLExperiment
 from experiment_lab.core import run_experiment
+
+from emma.poi_field import POIFieldModel
+from emma.external_model import ExternalModelTrainer
+
+
+logger = logging.getLogger(__name__)
+
+
+class ExternalModelTrainerCallback(BaseCallback):
+
+    def __init__(
+        self,
+        model_trainer: ExternalModelTrainer,
+        poi_field_model: POIFieldModel,
+        batch_size: int = 32,
+        wandb_mode: str | None = None,
+        verbose: int = 0,
+    ):
+        super().__init__(verbose)
+        self.model_trainer = model_trainer
+        self.poi_field_model = poi_field_model
+        self.batch_size = batch_size
+        self.wandb_mode = wandb_mode
+
+    def _on_step(self) -> bool:
+        return super()._on_step()
+
+    def _on_training_start(self) -> None:
+        return super()._on_training_start()
+
+    def _on_training_end(self) -> None:
+        return super()._on_training_end()
+
+    def _on_rollout_start(self) -> None:
+        return super()._on_rollout_start()
+
+    def _on_rollout_end(self) -> None:
+        super()._on_rollout_end()
+        env = self.model.get_env()
+
+        av_loss = self.model_trainer.receive_rollout(
+            env=env,
+            rollout_buffer=self.model.rollout_buffer,
+            info_buffer=self.model.info_buffer,
+        )
+
+        av_poi_value = self.poi_field_model.calculate_poi_values(
+            env=env,
+            rollout_buffer=self.model.rollout_buffer,
+            info_buffer=self.model.info_buffer,
+        ).mean()
+
+        logger.info(f"Loss: {av_loss}; Av POI per step: {av_poi_value}")
+        if self.wandb_mode != "disabled":
+            wandb.log(
+                {
+                    "external_model_loss": av_loss,
+                    "av_poi_per_step": av_poi_value,
+                }
+            )
 
 
 @dataclass
@@ -41,6 +105,7 @@ class EMMAConfig(RLConfig):
             0,
             {
                 "model_trainer": model_trainer,
+                "poi_field_model": instantiated_poi_model,
                 "batch_size": self.external_model_batch_size,
                 "wandb_mode": (
                     self.wandb["mode"]
--- emma/external_model.py
+++ emma/external_model.py
@@ -3,7 +3,6 @@ from typing import Dict, Type
 import abc
 import torch
 import logging
-import wandb
 import numpy as np
 
 from stable_baselines3.common.callbacks import BaseCallback
@@ -67,58 +66,10 @@ class ExternalModelTrainer(abc.ABC):
         self.optimizer.zero_grad()
 
         self.model.train(mode=True)
-        pred_out = self.predict(inp=inp)
+        pred_out = self.model(inp)
         loss = self.loss_f(out, pred_out)
         loss.backward()
 
         self.optimizer.step()
 
         return loss.item()
-
-    def predict(self, inp: torch.Tensor) -> torch.Tensor:
-        return self.model(inp)
-
-
-class ExternalModelTrainerCallback(BaseCallback):
-
-    def __init__(
-        self,
-        model_trainer: ExternalModelTrainer,
-        batch_size: int = 32,
-        wandb_mode: str | None = None,
-        verbose: int = 0,
-    ):
-        super().__init__(verbose)
-        self.model_trainer = model_trainer
-        self.batch_size = batch_size
-        self.wandb_mode = wandb_mode
-
-    def _on_step(self) -> bool:
-        return super()._on_step()
-
-    def _on_training_start(self) -> None:
-        return super()._on_training_start()
-
-    def _on_training_end(self) -> None:
-        return super()._on_training_end()
-
-    def _on_rollout_start(self) -> None:
-        return super()._on_rollout_start()
-
-    def _on_rollout_end(self) -> None:
-        super()._on_rollout_end()
-        env = self.model.get_env()
-
-        av_loss = self.model_trainer.receive_rollout(
-            env=env,
-            rollout_buffer=self.model.rollout_buffer,
-            info_buffer=self.model.info_buffer,
-        )
-
-        logger.info(f"Loss: {av_loss}")
-        if self.wandb_mode != "disabled":
-            wandb.log(
-                {
-                    "external_model_loss": av_loss,
-                }
-            )
