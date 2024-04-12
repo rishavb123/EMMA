f73ec01ce27322ecee31d9091fc38bf426b09f1d
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-04-12 00:56:28 -0400

    reordered classes

--- emma/experiment.py
+++ emma/experiment.py
@@ -25,6 +25,79 @@ from emma.poi_wrapper import StateSampler
 logger = logging.getLogger(__name__)
 
 
+@dataclass
+class EMMAConfig(RLConfig):
+
+    model_trainer: Dict[str, Any] = MISSING
+    external_model_batch_size: int = 64
+    n_eval_steps: int = 1280
+
+    poi_model: Dict[str, Any] = field(
+        default_factory=lambda: {"_target_": "emma.poi_field.ZeroPOIField"}
+    )
+    state_sampler: Dict[str, Any] | None = field(
+        default_factory=lambda: {"_target_": "emma.state_samplers.NoiseSampler"}
+    )
+    emma_wrapper_kwargs: Dict[str, Any] = field(default_factory=lambda: {})
+
+    def __post_init__(self) -> None:
+        super().__post_init__()
+
+        model_trainer = hydra.utils.instantiate(
+            self.model_trainer,
+            device=self.device,
+        )
+        instantiated_poi_model = hydra.utils.instantiate(
+            self.poi_model, external_model_trainer=model_trainer
+        )
+        instantiated_state_sampler = hydra.utils.instantiate(
+            self.state_sampler,
+            device=self.device,
+        )
+
+        if self.callback_cls_lst is None:
+            self.callback_cls_lst = []
+        self.callback_cls_lst.insert(0, "emma.experiment.ExternalModelTrainerCallback")
+
+        if self.callback_kwargs_lst is None:
+            self.callback_kwargs_lst = []
+        self.callback_kwargs_lst.insert(
+            0,
+            {
+                "model_trainer": model_trainer,
+                "poi_field_model": instantiated_poi_model,
+                "state_sampler": instantiated_state_sampler,
+                "batch_size": self.external_model_batch_size,
+                "n_eval_steps": self.n_eval_steps,
+                "wandb_mode": (
+                    self.wandb["mode"]
+                    if self.wandb is not None and "mode" in self.wandb
+                    else (None if self.wandb is not None else "disabled")
+                ),
+            },
+        )
+
+        if self.wrapper_cls_lst is None:
+            self.wrapper_cls_lst = []
+        if self.wrapper_kwargs_lst is None:
+            self.wrapper_kwargs_lst = []
+        for _ in range(len(self.wrapper_kwargs_lst), len(self.wrapper_cls_lst)):
+            self.wrapper_kwargs_lst.append({})
+        self.wrapper_cls_lst.append("emma.poi_wrapper.EMMAWrapper")
+        self.wrapper_kwargs_lst.append(
+            {
+                "poi_model": instantiated_poi_model,
+                "state_sampler": instantiated_state_sampler,
+                **self.emma_wrapper_kwargs,
+            }
+        )
+
+        if self.model_kwargs is None:
+            self.model_kwargs = {"poi_model": instantiated_poi_model}
+        else:
+            self.model_kwargs["poi_model"] = instantiated_poi_model
+
+
 class ExternalModelTrainerCallback(BaseCallback):
 
     def __init__(
@@ -161,79 +234,6 @@ class ExternalModelTrainerCallback(BaseCallback):
             if eval_av_poi_value is not None:
                 wandb_logs["external_model_eval/av_poi_per_step"] = eval_av_poi_value
             wandb.log(wandb_logs)
-
-
-@dataclass
-class EMMAConfig(RLConfig):
-
-    model_trainer: Dict[str, Any] = MISSING
-    external_model_batch_size: int = 64
-    n_eval_steps: int = 1280
-
-    poi_model: Dict[str, Any] = field(
-        default_factory=lambda: {"_target_": "emma.poi_field.ZeroPOIField"}
-    )
-    state_sampler: Dict[str, Any] | None = field(
-        default_factory=lambda: {"_target_": "emma.state_samplers.NoiseSampler"}
-    )
-    emma_wrapper_kwargs: Dict[str, Any] = field(default_factory=lambda: {})
-
-    def __post_init__(self) -> None:
-        super().__post_init__()
-
-        model_trainer = hydra.utils.instantiate(
-            self.model_trainer,
-            device=self.device,
-        )
-        instantiated_poi_model = hydra.utils.instantiate(
-            self.poi_model, external_model_trainer=model_trainer
-        )
-        instantiated_state_sampler = hydra.utils.instantiate(
-            self.state_sampler,
-            device=self.device,
-        )
-
-        if self.callback_cls_lst is None:
-            self.callback_cls_lst = []
-        self.callback_cls_lst.insert(0, "emma.experiment.ExternalModelTrainerCallback")
-
-        if self.callback_kwargs_lst is None:
-            self.callback_kwargs_lst = []
-        self.callback_kwargs_lst.insert(
-            0,
-            {
-                "model_trainer": model_trainer,
-                "poi_field_model": instantiated_poi_model,
-                "state_sampler": instantiated_state_sampler,
-                "batch_size": self.external_model_batch_size,
-                "n_eval_steps": self.n_eval_steps,
-                "wandb_mode": (
-                    self.wandb["mode"]
-                    if self.wandb is not None and "mode" in self.wandb
-                    else (None if self.wandb is not None else "disabled")
-                ),
-            },
-        )
-
-        if self.wrapper_cls_lst is None:
-            self.wrapper_cls_lst = []
-        if self.wrapper_kwargs_lst is None:
-            self.wrapper_kwargs_lst = []
-        for _ in range(len(self.wrapper_kwargs_lst), len(self.wrapper_cls_lst)):
-            self.wrapper_kwargs_lst.append({})
-        self.wrapper_cls_lst.append("emma.poi_wrapper.EMMAWrapper")
-        self.wrapper_kwargs_lst.append(
-            {
-                "poi_model": instantiated_poi_model,
-                "state_sampler": instantiated_state_sampler,
-                **self.emma_wrapper_kwargs,
-            }
-        )
-
-        if self.model_kwargs is None:
-            self.model_kwargs = {"poi_model": instantiated_poi_model}
-        else:
-            self.model_kwargs["poi_model"] = instantiated_poi_model
 
 
 class EMMAAnalysis(BaseAnalysis):
